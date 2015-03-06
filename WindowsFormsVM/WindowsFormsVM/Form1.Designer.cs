62106257040ad70b218680cf8a072b7e7a382945
Carlos Eduardo Farias Ribeiro <carlosribeiro@cejoseleitelopes.com.br>
2015-03-06 13:18:04 -0300

    Iterativo e Recursivo
    Trabalho sobre iteratividade e recursividade.

---
+++ WindowsFormsVM/WindowsFormsVM/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace WindowsFormsVM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.caixa = new System.Windows.Forms.TextBox();
+            this.label = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(13, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(101, 54);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "interativo";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Interativo_bt);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(129, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 54);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "recursivo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.recursivo_bt);
+            // 
+            // caixa
+            // 
+            this.caixa.Location = new System.Drawing.Point(13, 13);
+            this.caixa.Name = "caixa";
+            this.caixa.Size = new System.Drawing.Size(217, 20);
+            this.caixa.TabIndex = 2;
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Location = new System.Drawing.Point(237, 19);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(71, 13);
+            this.label.TabIndex = 3;
+            this.label.Text = "usaheusaheu";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(301, 134);
+            this.Controls.Add(this.label);
+            this.Controls.Add(this.caixa);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox caixa;
+        private System.Windows.Forms.Label label;
+    }
+}
+
---
+++ WindowsFormsVM/WindowsFormsVM/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsVM
+{
+    public partial class Form1 : Form
+    {
+        public Int64 n;
+        double resultado;
+        public List<Int64> numbers = new List<Int64>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Interativo_bt(object sender, EventArgs e)
+        {
+            resultado = iterativo_fact(Double.Parse(caixa.Text));
+            
+            label.Text = Convert.ToString(resultado);
+        }
+        
+        private void recursivo_bt(object sender, EventArgs e)
+        {
+            resultado = recursivo_fact(Double.Parse(caixa.Text));
+            label.Text = Convert.ToString(resultado);
+        }
+
+       
+        private double iterativo_fact(double value)
+        {
+            double num = value;
+            for (double i = num - 1; i > 1; i--)
+            {
+                num *= i;
+            }
+            Debug.Print(Convert.ToString(num));
+            return num;
+        
+        }
+       
+        private double recursivo_fact(double value)
+        {
+            double num = value;
+            if (num <= 1) return num = 1;
+            return num = num * recursivo_fact(num - 1);
+        }
+
+
+
+       
+
+    }
+}
